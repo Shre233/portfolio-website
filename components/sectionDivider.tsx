768bd048c51e97cfda2580d4b6682ac39a52c91e
Shreyansh <jainshreyansh233@gmail.com>
2024-03-29 00:37:37 +0530

    Bug2

--- components/sectionDivider.tsx
+++ components/sectionDivider.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import { motion } from "framer-motion"
 export default function
-    () {
+    SectionDivider() {
     return (
         <motion.div className='bg-gray-200 my-[3rem] h-16 w-1 rounded-full
                             hidden sm:block dark:bg-opacity-20'
